a9b8c836eb18334ae64bf951139f179111d174c6
louis <casbas.lou@gmail.com>
2023-03-29 22:55:51 +0800

    Bonds and zero coupon bonds

--- pages/07_Basic_instruments.py
+++ pages/07_Basic_instruments.py
@@ -12,8 +12,8 @@ st.markdown(
     'are important in the valuation of derivative')
 
 st.markdown("In the topic of fixed income we will talk about essential debt instruments known as zero coupon."
-        "Then we will talk about the basics of Equity and CCY market : features of stocks and factor impacting their future prices."
-        "We will discuss how CCY cant be seen as a stock asset, forward and future")
+            "Then we will talk about the basics of Equity and CCY market : features of stocks and factor impacting their future prices."
+            "We will discuss how CCY cant be seen as a stock asset, forward and future")
 
 st.markdown(
     "Finally we will talk about swap and how they are central in the OTC market (the most traded one being the interest rate swap)"
@@ -44,17 +44,18 @@ st.markdown(
 
 st.subheader("Yield Curve")
 
-st.markdown("For major ccy the interest rate paid on bond swap or future are plotted against their maturities, Those graphs are called Yield curves"
-            "there is corporate curves that correspond to yields of bond issued by companies. Because their is higher credit risk"
-            "thoses curves are usually higher and quoted in term of credit spread over libor. They are usually expressed as an annual rate "
-            "to compare them easly. They are usually upward sloping with longer term rate higher than short term")
-
+st.markdown(
+    "For major ccy the interest rate paid on bond swap or future are plotted against their maturities, Those graphs are called Yield curves"
+    "there is corporate curves that correspond to yields of bond issued by companies. Because their is higher credit risk"
+    "thoses curves are usually higher and quoted in term of credit spread over libor. They are usually expressed as an annual rate "
+    "to compare them easly. They are usually upward sloping with longer term rate higher than short term")
 
 st.subheader("Time value of money")
 
-st.markdown("An investor would prefer take possession of an amount of money today. In order to earn an interest of this money"
-            "if we plan to pay me in one year, and r = 5 i could have earn 5 in this period of time. Therefore i discount it and concider that the present value is"
-            "5 unit loosed ")
+st.markdown(
+    "An investor would prefer take possession of an amount of money today. In order to earn an interest of this money"
+    "if we plan to pay me in one year, and r = 5 i could have earn 5 in this period of time. Therefore i discount it and concider that the present value is"
+    "5 unit loosed ")
 
 st.text("100 dollard future value expressed in today value if rate =5% per year is :")
 st.latex('PV = FV * 1/(1 + i)^n')
@@ -62,20 +63,64 @@ st.latex('100/105')
 st.latex('= 95.24')
 st.text("n being the number of periods over witch we compound the interest")
 
-st.markdown('Also, in this example it was an annual rate applied over a 1 year period. But we can think of coumponding as applying the rate to one period'
-            'and reinvesting the result for another period and so on.'
-            'So the presetn value at time 0 of a payment at time t in the future is given in term of future value')
+st.markdown(
+    'Also, in this example it was an annual rate applied over a 1 year period. But we can think of coumponding as applying the rate to one period'
+    'and reinvesting the result for another period and so on.'
+    'So the presetn value at time 0 of a payment at time t in the future is given in term of future value')
 
 st.latex('PV = FVe^-rt')
 
-exo = st.expander(label='Exercice : If you make a deposit of 100$ today and interest rate are constant and equals to 10%'
-                        'In the case of annual compounding how many years are needed for the value deposit to double to 200$')
+exo = st.expander(
+    label='Exercice : If you make a deposit of 100$ today and interest rate are constant and equals to 10%'
+          'In the case of annual compounding how many years are needed for the value deposit to double to 200$')
 with exo:
     st.latex("PV = FV * 1/(1 + i)^n")
     st.latex("n = ln(PV/FV) / ln(1 + i)")
     st.latex("n = ln(100/200) / ln(1 + 0,10)")
     st.markdown("7,27")
 
+st.markdown(
+    "An investor would prefer take possession of an amount of money today. In order to earn an interest of this money")
+st.subheader("Bonds")
 
+st.markdown(
+    'The bond is a debt securitie issued by governement and companies. In exchange of the funds, the holder is entilted to receive coupons paid periodicly'
+    'as well as the return of initial investment. The coupon can also be linked to an index, we then talk about floating rate note'
+    'The market price of a bond is equal to the sum of the present value of expecte cashflow')
 
-st.markdown("An investor would prefer take possession of an amount of money today. In order to earn an interest of this money")
+st.text('t the variation rate and C the value that are still to be paid at coupon dates')
+
+st.latex(r'Bond(t,T) = \[ \sum_{n=1}^{n} CiB(t,ti)\]')
+st.text('witch is equal to')
+st.latex(r'Bond(t,T) = \[ \sum_{n=1}^{n} Cie^{-r(t,ti) * (ti-t)}\]')
+st.text('expressed in term of yield to maturity')
+st.latex(r'Bond(t,T) = \[ \sum_{n=1}^{n} Cie^{-y(ti-t)}\]')
+
+st.markdown(
+    "The price of a bond may include the interest rate that has accrued since the last coupon date"
+    "The price including accrued interest is known as dirty price and correspond to the fair value of the bond"
+    "Quoted bond display clean prices witch doesn't include accrued interest")
+st.latex('Clean price = Dirty price - accrued interest')
+
+st.markdown('bonds are concidered to be safer instruments than stocks, Bonds can pay higher interest compare to stocks dividend'
+            'Also bonds suffer from less liquidity issues than stocks'
+            'Nonetheless bond are not risk free, because they are a direct function of interest rate. A rise of the market interest'
+            'decrease values of bonds. If the interest rate used to discount the coupon goes up their present value goes down'
+            'Also Bond price depend on the credit rating of the issuer if the risk of credit increase the value of bond decrease. '
+            'Therefore credit risk increase volatility of bond prices. In the case of callable bonds, the bond can be called'
+            'ie bought back by the issuer. Buying a callable bond is equivalent to buying a bond and selling an american call option on this bond.'
+            'When interest rate go down, price of bond increase and issuer tend to exercice their call and buy back the bond')
+
+st.subheader("Zero coupon Bonds")
+
+st.markdown('Zero coupon bonds are debpt investments when the lender receives back a principal amount (also called notional) '
+            'plus interest only at matu. No coupon are paid during the life of the product.'
+            'The interst is deduced upfront and reflected in the price, wich means that is price is lower than 100% of the notional'
+            'it is better in a medium term liquidity perspective because no payment have to be made in the meantime'
+            'The price of such an bond is equal to the present value of the par value')
+st.latex('Linear : interest is proportional to the lengh of the loan')
+st.latex('B(t,T) = 1/1+ r(t,T) * (T-t)')
+st.latex('Actuarial : interest is compounded periodically')
+st.latex('B(t,T) = 1/1+ r(t,T))^{(T-t)}')
+st.latex('Continuous : interest is compounded continuously')
+st.latex('B(t,T) =e^{-r(t,T) x (T-t)')
