8cf690bf2ba0d21e29818564cbd89c8ddb1a0f57
louis <casbas.lou@gmail.com>
2023-04-05 17:17:52 +0800

    trader and stucturers

--- pages/the_world_of_structured_product.py
+++ pages/the_world_of_structured_product.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 chap = ['Introduction', 'The structured note', 'Libor Vs Treasury rate', 'Yield Curve', 'Time value of money',
-        'The structured note', 'The sell side']
+        'The structured note', 'The sell side','Traders and structurers','The buy side']
 
 tabs = st.tabs(chap)
 for tab_name, tab in zip(chap, tabs):
@@ -31,3 +31,23 @@ for tab_name, tab in zip(chap, tabs):
             st.markdown(
                 'backtesting is a technique used by sells to show the potential benefit a client would have had '
                 'stress test are used to show that even in a crash scenario the product react well')
+    elif tab_name =='Traders and structurers':
+        with tab:
+            st.header(" **Traders and structurers** ")
+            st.markdown(
+                'The role of strctu varies from one institution to another but generally involves creating new strctures'
+                'as well as pricing. It involves analysing the risks of suchs products. After a deal is sealed it goes '
+                'to the trader portoflio who will be in charge to hedge the risks.'
+                'Vanilla products are typically managed by volatility trader.'
+                'The role of exo trader is to hedge. The more complex the product the more elaborate the risk')
+            st.markdown('sales are interested in settling as many deals as possible to increase their commision')
+
+    elif tab_name == 'The buy side':
+        with tab:
+            st.header(" **'The buy side'** ")
+            st.subheader('to have meaning an exchange of cashflow involve more than one party. It can be classified '
+                         'in two cattegory : retail and instit')
+            st.title('retail')
+            st.markdown(
+                'it is usually asset management instit that buy structured products from investment bank and redistribute them '
+                'to individual')
